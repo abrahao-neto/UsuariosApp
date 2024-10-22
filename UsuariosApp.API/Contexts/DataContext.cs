8d1f4150a3821291f7a4ffd9a29387f1f063ec51
Abrahão Neto <abrahao.oliveira.neto@gmail.com>
2024-10-21 21:25:17 -0300

    first commit

---
+++ UsuariosApp.API/Contexts/DataContext.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UsuariosApp.API.Entities;
+
+namespace UsuariosApp.API.Contexts
+{
+    public class DataContext : DbContext
+    {
+        /// <summary>
+        /// Construtor para injeção de dependência do contexto
+        /// </summary>
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        /// <summary>
+        /// Método para adicionarmos os mapeamentos das entidades
+        /// </summary>
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            #region Mapeamento do usuário
+
+            modelBuilder.Entity<Usuario>()
+                 .HasKey(u => u.Id);
+
+            modelBuilder.Entity<Usuario>()
+                .Property(u => u.Nome)
+                .HasMaxLength(150)
+                .IsRequired();
+
+            modelBuilder.Entity<Usuario>()
+                .Property(u => u.Email)
+                .HasMaxLength(100)
+                .IsRequired();
+
+            modelBuilder.Entity<Usuario>()
+                .Property(u => u.Senha)
+                .HasMaxLength(100)
+                .IsRequired();
+
+            modelBuilder.Entity<Usuario>()
+                .Property(u => u.RefreshToken)
+                .HasMaxLength(100);
+
+            modelBuilder.Entity<Usuario>()
+                .Property(u => u.RefreshTokenExpiration);
+
+            modelBuilder.Entity<Usuario>()
+                .HasIndex(u => u.Email)
+                .IsUnique();
+
+            #endregion
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
+
+
+
---
+++ UsuariosApp.API/Extensions/EntityFrameworkExtension.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UsuariosApp.API.Contexts;
+
+namespace UsuariosApp.API.Extensions
+{
+    public static class EntityFrameworkExtension
+    {
+        public static IServiceCollection AddEntityFrameworkConfig(this IServiceCollection services, IConfiguration configuration)
+        {
+            //capturando a string de conexão do /appsetting
+            var connectionString = configuration.GetConnectionString("UsuariosApp");
+
+            //injeção de dependência do conteexto do EntityFramework
+            services.AddDbContext<DataContext>(options => options.UseSqlServer(connectionString));
+
+            return services;
+        }
+    }
+}
